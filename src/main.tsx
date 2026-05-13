682e0d1283230e9fd591656f4f1cfab82f482072
chai1808 <mojigyo.chinen@gmail.com>
2026-05-13 18:20:08 +0900

    fix:old js

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './assets/css/reset.css';
 import './assets/css/common.css';
 import './assets/css/add/lity.min.css';
+import './utils/common.funcs.js';
 
 import App from './App.tsx';
 
