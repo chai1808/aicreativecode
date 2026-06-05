ab59211b8d8ea63a5a4bdc08f0e588c82d695042
chai1808 <bluesouai0925@gmail.com>
2026-06-05 13:01:19 +0900

    i forgot to remove fragment

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, Fragment } from 'react';
+import { useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
